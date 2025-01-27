cc984d9df36253d070d35ae2d82b3f7952adb419
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2025-01-27 20:35:51 +0000

    instrumenting python apps with prometheus_client

--- error-app/error_app.py
+++ error-app/error_app.py
@@ -1,6 +1,11 @@
 import time
 import random
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
+from prometheus_client import Counter, Histogram, generate_latest
+
+view_metric = Counter('health_check_requests', 'Total number of health check requests') # health metric
+
+health_check_latency = Histogram("health_check_latency_seconds", "Latency of health check requests")
 
 app = Flask(__name__)
 
@@ -13,7 +18,13 @@ def health_check():
     if random.random() < 0.5:
         time.sleep(2)
 
+    # view metric
+    view_metric.inc()
     return jsonify({"status": "healthy"}), 200
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
